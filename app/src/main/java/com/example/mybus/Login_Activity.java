0c195fa79a50bb162b727db8424158d09f562bf3
Asmaaa7779 <asmaeelyakoubi096@gmail.com>
2025-03-16 02:41:44 +0000

    liaison entre login and sign up pages

--- app/src/main/java/com/example/mybus/Login_Activity.java
+++ app/src/main/java/com/example/mybus/Login_Activity.java
@@ -1,4 +1,26 @@
 package com.example.mybus;
 
-public class Login_Activity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Login_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_activity);
+        TextView textSignIn = findViewById(R.id.signupview);
+
+        textSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Login_Activity.this, SignUpActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
 }
--- app/src/main/java/com/example/mybus/SignUpActivity.java
+++ app/src/main/java/com/example/mybus/SignUpActivity.java
@@ -1,6 +1,8 @@
 package com.example.mybus;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -18,6 +20,11 @@ public class SignUpActivity extends AppCompatActivity {
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.signup), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            TextView textViewSignIn = findViewById(R.id.loginview);
+            textViewSignIn.setOnClickListener(view -> {
+                Intent intent = new Intent(SignUpActivity.this, Login_Activity.class);
+                startActivity(intent);
+            });
             return insets;
         });
     }
